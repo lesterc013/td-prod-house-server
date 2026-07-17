6226ef49ef625c9b698840091d96de578449cfee
lester-mtse-machine <lesterc013@gmail.com>
2026-07-17 10:31:42 +0800

    Use indexRouter for '/'

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 import express from 'express';
+import indexRouter from './src/routes/indexRouter.js';
 
 const PORT = process.env.SERVER_PORT;
 
 const app = express();
+
+app.use('/', indexRouter);
 
 app.listen(PORT, (error) => {
   if (error) {
