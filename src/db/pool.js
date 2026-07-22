c9d722d3a62346cd3e10ed838d9c7b6f411d7ccc
lester-mtse-machine <lesterc013@gmail.com>
2026-07-22 10:44:32 +0800

    Add pgvector registerTypes step

--- src/db/pool.js
+++ src/db/pool.js
@@ -1,5 +1,7 @@
 import { Pool } from 'pg';
+import pgvector from 'pgvector/pg';
 
 export default new Pool({
   connectionString: process.env.DATABASE_URL,
+  onConnect: async (client) => await pgvector.registerTypes(client),
 });
