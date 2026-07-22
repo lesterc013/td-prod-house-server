4251a5d5ff309c4c5b7a39c0a6b0b86d975f2bc9
lester-mtse-machine <lesterc013@gmail.com>
2026-07-22 10:45:22 +0800

    Convert embeddings top correct postgres expected vector type

--- src/db/chunksQueries.js
+++ src/db/chunksQueries.js
@@ -1,9 +1,10 @@
 import pool from './pool.js';
+import pgvector from 'pgvector/pg';
 
 export async function insertChunkDb(documentId, content, embedding) {
   const res = await pool.query(
     'INSERT INTO chunks (document_id, content, embedding) VALUES ($1, $2, $3) RETURNING id',
-    [documentId, content, embedding],
+    [documentId, content, pgvector.toSql(embedding)],
   );
   return res.rows[0].id;
 }
