8acd0e34c5cb0fc4bb83232fcac9ceaf8d11dd0c
lester-mtse-machine <lesterc013@gmail.com>
2026-07-28 11:34:21 +0800

    refactor: Replace normal chunk splitter with markdown based

--- src/services/textChunker.js
+++ src/services/textChunker.js
@@ -1,18 +1,23 @@
-import { RecursiveCharacterTextSplitter } from '@langchain/textsplitters';
+import { MarkdownTextSplitter } from '@langchain/textsplitters';
 
 // Note: chunk related params are for num of chars per langchain docs.
-const CHUNK_SIZE = 1000;
+const CHUNK_SIZE = 2000;
 const CHUNK_OVERLAP = 200;
-const splitter = new RecursiveCharacterTextSplitter({
+
+const markdownSplitter = new MarkdownTextSplitter({
   chunkSize: CHUNK_SIZE,
   chunkOverlap: CHUNK_OVERLAP,
+  keepSeparator: true,
 });
 
 /**
- * Returns a string[] containing each of the chunks.
- * @param {string} text
+ *
+ * @param {string} markdown - a string of the file parsed as markdown format.
+ * @returns {string[]} Array of chunks by markdown i.e. section headers then the chunk size.
  */
-export async function chunkText(text) {
-  const chunks = await splitter.splitText(text);
-  return chunks;
+export async function splitMarkdown(markdown) {
+  const chunks = await markdownSplitter.splitText(markdown);
+  // Only leave chunks that have > 100 chars
+  // This is due to noise like <--image--> or image captions that are unnecessary
+  return chunks.filter((chunk) => chunk.trim().length > 100);
 }
