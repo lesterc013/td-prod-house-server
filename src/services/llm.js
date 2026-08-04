755dc915af3f7264bfec3b66c6fb95a961605f0f
lester-mtse-machine <lesterc013@gmail.com>
2026-08-04 16:44:05 +0800

    Wrap llm async logic in try/catch

--- src/services/llm.js
+++ src/services/llm.js
@@ -6,26 +6,32 @@ import AppError from '../utils/AppError.js';
  * @returns number[] that contains the required embeddings.
  */
 export async function requestEmbedding(input) {
-  const res = await fetch(process.env.EMBEDDING_ENDPOINT, {
-    method: 'POST',
-    body: JSON.stringify({
-      model: process.env.EMBEDDING_MODEL_NAME,
-      input,
-    }),
-  });
+  try {
+    const res = await fetch(process.env.EMBEDDING_ENDPOINT, {
+      method: 'POST',
+      body: JSON.stringify({
+        model: process.env.EMBEDDING_MODEL_NAME,
+        input,
+      }),
+    });
 
-  // Need a res.ok check cos with fetch, status codes like 404 or 500 dont throw errors,
-  // as the promise is still considered resolved.
-  if (!res.ok) {
+    // Need a res.ok check cos with fetch, status codes like 404 or 500 dont throw errors,
+    // as the promise is still considered resolved.
+    if (!res.ok) {
+      throw new Error(
+        `Embedding request failed. Response original text: ${res.statusText}`,
+      );
+    }
+
+    const data = await res.json();
+    return data.embeddings[0];
+  } catch (error) {
     throw new AppError(
-      `Embedding request failed. Response original text: ${res.statusText} — is ngrok running?`,
-      res.status,
-      null,
+      `llm service error: ${error.message}`,
+      error.statusCode ?? 503,
+      error,
     );
   }
-
-  const data = await res.json();
-  return data.embeddings[0];
 }
 
 /**
@@ -54,26 +60,32 @@ export async function generateAnswer(query, kChunks) {
     `Document excerpts (average confidence score: ${avgConfidenceScore})\n` +
     listOfChunks;
 
-  // Send to /api/generate
-  const res = await fetch(process.env.GENERATE_ENDPOINT, {
-    method: 'POST',
-    body: JSON.stringify({
-      model: process.env.GENERATE_MODEL_NAME,
-      prompt,
-      stream: false,
-    }),
-  });
+  try {
+    // Send to /api/generate
+    const res = await fetch(process.env.GENERATE_ENDPOINT, {
+      method: 'POST',
+      body: JSON.stringify({
+        model: process.env.GENERATE_MODEL_NAME,
+        prompt,
+        stream: false,
+      }),
+    });
 
-  // Need a res.ok check cos with fetch, status codes like 404 or 500 dont throw errors,
-  // as the promise is still considered resolved.
-  if (!res.ok) {
+    // Need a res.ok check cos with fetch, status codes like 404 or 500 dont throw errors,
+    // as the promise is still considered resolved.
+    if (!res.ok) {
+      throw new Error(
+        `Generate request failed. Response original text: ${res.statusText}`,
+      );
+    }
+
+    const data = await res.json();
+    return data.response;
+  } catch (error) {
     throw new AppError(
-      `Generate request failed. Response original text: ${res.statusText}`,
-      res.status,
-      null,
+      `llm service error: ${error.message}`,
+      error.statusCode ?? 503,
+      error,
     );
   }
-
-  const data = await res.json();
-  return data.response;
 }
